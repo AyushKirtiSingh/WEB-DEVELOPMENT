81d5811dab178bc79a6823678c597ae1b057b6c3
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-06 17:17:55 -0700

    video 55 completed

---
+++ video 55/script.js
@@ -0,0 +1,43 @@
+console.log("Hey thi si tutorial 55")
+
+let a = 5;
+a = a + 1
+let b = 10;
+let c = "Harry";
+let _a = "shubham";
+
+// var 55a = "rohan"      //not allowed
+
+console.log(a+b+8)
+console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 77;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 8           //not allowed because a1 is constant
+// a1 = a1 + 1
+
+let x = "harry bhai";
+let y = 22;
+let z = 3.55;
+const q = true;
+let p = undefined;
+let r = null;
+
+console.log(x,y,z,q,p,r);
+console.log(typeof x,typeof y,typeof z,typeof q,typeof p,typeof r);
+
+let o = {
+    "name" : "Ayush",
+    "Job code" : 5600,
+    "is_handsome" : true
+}
+console.log(o)
+
+o.salary = "100crores";
+console.log(o)
+o.salary = "500crores";
+console.log(o)
