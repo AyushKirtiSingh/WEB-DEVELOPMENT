509d503fc450e4c85ad67a2ccbb60817ad00bd70
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-08-06 17:18:46 -0700

    video 57 completed

---
+++ video 57/index.js
@@ -0,0 +1,42 @@
+console.log("I am a tutorial on loops")
+
+// let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for(let i=0;i<100;i++){
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name : "harry",
+//     role : "programmer",
+//     company : "codewithharry ai"
+// }
+
+// for (const key in obj){
+//     const element = obj[key];         //for in loop
+//     console.log(key,element);
+// }
+
+// for (const c of "harry") {            //for of loop
+//     console.log(c);
+// }
+    
+
+// let i=0;
+// while(i<6){
+//     console.log(i);
+//     i++;
+// }
+
+let i=10;
+do {
+    console.log(i);
+    i++;
+    
+} while (i<6);
+    
+    
+    
