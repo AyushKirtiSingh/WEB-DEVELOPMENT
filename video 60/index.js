7c07a0cf610a4509e12a6a084effbeec4d3bca7c
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-08 21:07:45 -0700

    video 60 completed

---
+++ video 60/index.js
@@ -0,0 +1,30 @@
+console.log("This is strings tutorial")
+let a = "harry"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[5]) wrong
+
+let real_name = "harry"
+let friend = "ayush"
+
+console.log("his name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "Ayush"
+
+console.log(b.length)
+console.log(b.toLowerCase())
+console.log(b.toUpperCase())
+console.log(b.slice(2,4))
+console.log(b.slice(2))
+console.log(b.replace("Ay","44"))
+console.log(b.concat(" fuck dishita"," and tushti fuck"," ayush"))
+console.log(b.charAt(2))
+console.log(b.startsWith("ay"))
+console.log(b.startsWith("Ay"))
+console.log(b.endsWith("h"))
+
+console.log(b)          //same as strings are immutable
