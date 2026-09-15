4490b553ed9c03ac54fb27703d16189cdc660c9d
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-15 15:06:31 -0700

    video 65 completed

---
+++ video 65/index.js
@@ -0,0 +1,19 @@
+let a = 6
+
+function factorial(number) {
+    let arr = Array.from(Array(number + 1).keys())
+    let c = arr.slice(1,).reduce((a, b)=> a * b)
+    return c
+
+}
+
+function facfor(number){
+    let fact = 1;
+    for (let index = 1; index <= number; index++) {
+        fact = fact * index;
+    }
+    return fact
+}
+
+console.log(factorial(a))
+console.log(facfor(a))
