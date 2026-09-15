45e54bee25c261fa80b1b03951bb5c368d27ff2c
AyushKirtiSingh9904 <ayushsingh9904@gmail.com>
2026-09-15 15:07:36 -0700

    video 67 completed

---
+++ video 67/script.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+document.body.firstElementChild.children[1].nextElementSibling
+document.body.firstElementChild.children[1].prevElementSibling
+
+document.body.children[1].rows    //access table rows
